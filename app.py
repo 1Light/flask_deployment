02bc3e2866cd8d98832970001460d21af256ea85
1Light <redclover1.618@gmail.com>
2024-07-09 16:31:26 +0300

    upload

--- app.py
+++ app.py
@@ -60,6 +60,9 @@ def login():
             return "Errrrror"
     return render_template('login.html')
 
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+     return render_template('home.html')
 
-""" if __name__ == '__main__':
-    app.run(debug=True) """
+if __name__ == '__main__':
+    app.run(debug=True)
